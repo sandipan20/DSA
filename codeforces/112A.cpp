378c45cd60ca1503df557d446a18be7f618a8db0
sandipan20 <sandipan2097@gmail.com>
2025-12-25 22:58:20 +0530

    25th dec 2025

--- codeforces/112A.cpp
+++ codeforces/112A.cpp
@@ -1,10 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <cctype>
 using namespace std;
-int main(){
-    string a,b;
-    cin>>a>>b;
-    for(int i=0;i<a.size();i++){
-        
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for (int i = 0; i < s1.length(); i++) {
+        char c1 = tolower(s1[i]);
+        char c2 = tolower(s2[i]);
+
+        if (c1 < c2) {
+            cout << -1 << endl;
+            return 0;
+        } 
+        else if (c1 > c2) {
+            cout << 1 << endl;
+            return 0;
+        }
     }
+
+    cout << 0 << endl;
+    return 0;
 }
