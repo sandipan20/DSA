ed71bdb9adf76a3b123cb6539381b0c95f0837aa
sandipan20 <sandipan2097@gmail.com>
2026-01-21 14:16:24 +0530

    21th jan 2026

---
+++ LeetCode/225_implement_stack_using_queue.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using namespace std;
+class MyStack {
+    queue<int> q1,q2;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        if(!q1.empty()){
+            q1.push(x);
+        } else{
+            q2.push(x);
+        }
+    }
+    
+    int pop() {
+        if(q1.empty()){
+            int s=(q2.size()-1);
+            while(s--){
+                q1.push(q2.front());
+                q2.pop();
+            }
+            int r=q2.front();
+            q2.pop();
+            return r;
+        } else{
+            int s=(q1.size()-1);
+            while(s--){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            int r=q1.front();
+            q1.pop();
+            return r;
+        }
+    }
+    
+    int top() {
+        if(q1.empty()){
+            int s=(q2.size()-1);
+            while(s--){
+                q1.push(q2.front());
+                q2.pop();
+            }
+            int r=q2.front();
+            q1.push(r);
+            q2.pop();
+            return r;
+        } else{
+            int s=(q1.size()-1);
+            while(s--){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            int r=q1.front();
+            q2.push(r);
+            q1.pop();
+            return r;
+        }
+    }
+    
+    bool empty() {
+        return q1.empty()&&q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+/*Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+ 
+
+Follow-up: Can you implement the stack using only one queue?*/
