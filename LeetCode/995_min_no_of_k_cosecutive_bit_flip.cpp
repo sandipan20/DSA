e880be66fe3ed213d6e2c3875cb55575e2692dd8
sandipan20 <sandipan2097@gmail.com>
2026-01-27 09:48:50 +0530

    27th jan 2026

---
+++ LeetCode/995_min_no_of_k_cosecutive_bit_flip.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        queue<int> q;
+        int flip=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(!q.empty() && q.front()<i) q.pop();
+            if((q.size()&1)==nums[i]){
+                //flip
+                if(i+k>n) return -1;
+                q.push(i+k-1);
+                flip++;
+            }
+        }
+        return flip;
+    }
+};
+
+/*You are given a binary array nums and an integer k.
+
+A k-bit flip is choosing a subarray of length k from nums and simultaneously changing every 0 in the subarray to 1, and every 1 in the subarray to 0.
+
+Return the minimum number of k-bit flips required so that there is no 0 in the array. If it is not possible, return -1.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0], k = 1
+Output: 2
+Explanation: Flip nums[0], then flip nums[2].
+Example 2:
+
+Input: nums = [1,1,0], k = 2
+Output: -1
+Explanation: No matter how we flip subarrays of size 2, we cannot make the array become [1,1,1].
+Example 3:
+
+Input: nums = [0,0,0,1,0,1,1,0], k = 3
+Output: 3
+Explanation: 
+Flip nums[0],nums[1],nums[2]: nums becomes [1,1,1,1,0,1,1,0]
+Flip nums[4],nums[5],nums[6]: nums becomes [1,1,1,1,1,0,0,0]
+Flip nums[5],nums[6],nums[7]: nums becomes [1,1,1,1,1,1,1,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= k <= nums.length*/
