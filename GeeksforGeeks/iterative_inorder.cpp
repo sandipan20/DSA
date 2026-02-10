ca62c61a68f76296803f861b93d196ff0384089b
sandipan20 <sandipan2097@gmail.com>
2026-02-10 23:51:10 +0530

    10th feb 2026

---
+++ GeeksforGeeks/iterative_inorder.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+class Solution {
+  public:
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        stack<Node*> st;
+        stack<bool> visited;
+        st.push(root);
+        visited.push(false);
+        while(!st.empty()){
+            Node* temp = st.top(); st.pop();
+            bool isVisited = visited.top(); visited.pop();
+            if(temp == NULL) continue;
+            if(isVisited){
+                ans.push_back(temp->data);
+            } else {
+                if(temp->right){
+                    st.push(temp->right);
+                    visited.push(false);
+                }
+                st.push(temp);
+                visited.push(true);
+                if(temp->left){
+                    st.push(temp->left);
+                    visited.push(false);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+/*Given a binary tree. Find the inorder traversal of the tree without using recursion.
+
+Examples
+
+Input:  
+
+Output: 4 2 5 1 3
+Explanation:
+Inorder traversal (Left->Root->Right) of 
+the tree is 4 2 5 1 3.
+Input:
+
+Output: 1 7 10 8 6 10 5 6
+Explanation:
+Inorder traversal (Left->Root->Right) 
+of the tree is 1 7 10 8 6 10 5 6.
+ 
+
+Your task:
+You don't need to read input or print anything. Your task is to complete the function inOrder() which takes the root of the tree as input and returns a list containing the inorder traversal of the tree, calculated without using recursion.
+
+Expected time complexity: O(N)
+Expected auxiliary space: O(N)
+
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+*/
---
+++ GeeksforGeeks/postorder_iterative.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+class Solution {
+  public:
+    vector<int> postOrder(Node* root) {
+        // code here
+        stack<Node*> st;
+        vector<int> ans;
+        st.push(root);
+        while(!st.empty()){
+            Node*temp=st.top();
+            st.pop();
+            ans.push_back(temp->data);
+            if(temp->left) st.push(temp->left);
+            if(temp->right) st.push(temp->right);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+/*Given a binary tree. Find the postorder traversal of the tree without using recursion. Return a list containing the postorder traversal of the tree, calculated without using recursion.
+
+Examples :
+
+Input:
+           1
+         /   \
+        2     3
+      /  \
+     4    5
+
+Output: 4 5 2 3 1
+Explanation: Postorder traversal (Left->Right->Root) of the tree is 4 5 2 3 1.
+Input:
+             8
+          /      \
+        1          5
+         \       /   \
+          7     10    6
+           \   /
+            10 6
+
+Output: 10 7 1 6 10 6 5 8 
+Explanation: Postorder traversal (Left->Right->Root) of the tree is 10 7 1 6 10 6 5 8 .
+ 
+Expected time complexity: O(n)
+Expected auxiliary space: O(n)
+ 
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105*/
