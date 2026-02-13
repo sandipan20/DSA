f966202777fe9a6e4beaf316a43a5ee4fc8bc334
sandipan20 <sandipan2097@gmail.com>
2026-02-13 10:41:16 +0530

    13th feb 2026

---
+++ GeeksforGeeks/tree_boundry_traversal.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+  public:
+    void left(Node* root,vector<int> &ans){
+        if(!root||(!root->left&&!root->right )) return;
+        ans.push_back(root->data);
+        if(root->left){
+            left(root->left,ans);
+        } else{
+            left(root->right,ans);
+        }
+    }
+    void leaf(Node* root,vector<int> &ans){
+        if(!root ) return;
+        if(!root->left&&!root->right){
+            ans.push_back(root->data);
+            return;
+        }
+        leaf(root->left,ans);
+        leaf(root->right,ans);
+    }
+    void right(Node* root,vector<int> &ans){
+        if(!root||(!root->left&&!root->right )) return;
+        if(root->right){
+            right(root->right,ans);
+        } else{
+            right(root->left,ans);
+        }
+        ans.push_back(root->data);
+    }
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        //print root
+        vector<int>ans;
+        ans.push_back(root->data);
+        //print left only
+        left(root->left,ans);
+        //print leaf from left to right
+        if(root->left||root->right)
+        leaf(root,ans);
+        //print right only in reverse
+        right(root->right,ans);
+        return ans;
+    }
+};
+/*Given a root of a Binary Tree, return its boundary traversal in the following order:
+
+Left Boundary: Nodes from the root to the leftmost non-leaf node, preferring the left child over the right and excluding leaves.
+Leaf Nodes: All leaf nodes from left to right, covering every leaf in the tree.
+Reverse Right Boundary: Nodes from the root to the rightmost non-leaf node, preferring the right child over the left, excluding leaves, and added in reverse order.
+Note: The root is included once, leaves are added separately to avoid repetition, and the right boundary follows traversal preference not the path from the rightmost leaf.
+
+Examples:
+
+Input: root = [1, 2, 3, 4, 5, 6, 7, N, N, 8, 9, N, N, N, N]
+Output: [1, 2, 4, 8, 9, 6, 7, 3]
+Explanation:
+
+Input: root = [1, N, 2, N, 3, N, 4, N, N] 
+Output: [1, 4, 3, 2]
+Explanation:
+
+Left boundary: [1] (as there is no left subtree)
+Leaf nodes: [4]
+Right boundary: [3, 2] (in reverse order)
+Final traversal: [1, 4, 3, 2]
+Constraints:
+1 ≤ number of nodes ≤ 105
+1 ≤ node->data ≤ 105
+*/
