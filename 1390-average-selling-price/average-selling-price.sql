d4b7abe953faa374fe7d1d6f8fd40898e083130d
sandipan <sandipan2097@gmail.com>
2026-08-11 09:02:54 +0530

    Time: 840 ms (64.71%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,9 +1,6 @@
 SELECT 
     p.product_id,
-    ROUND(
-        IFNULL(SUM(p.price * u.units) / SUM(u.units), 0),
-        2
-    ) AS average_price
+    ROUND(IFNULL(SUM(p.price * u.units) / SUM(u.units), 0),2) AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
