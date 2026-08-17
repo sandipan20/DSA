f6e94940323447b55edfdae5d8a1a7d7a52b7e7a
sandipan <sandipan2097@gmail.com>
2026-08-17 10:43:06 +0530

    Time: 3 ms (64.72%) | Memory: 7.9 MB (21.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,12 +3,12 @@ public:
     int fib(int n) {
         if(n<=1)
             return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<n+1;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        int curr,prev=1,prev2=0;
+        for(int i=2;i<=n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return curr;
     }
 };
