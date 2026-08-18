33f9e34d754cc2542cc55b594c626f14cd49beed
sandipan <sandipan2097@gmail.com>
2026-08-18 10:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,21 @@
 class Solution {
-    int dyanamicrec(int i,int n,vector<int> &dp){
-        if(i==n)
-            return 1;
-        if(i>n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        return dp[i]=dyanamicrec(i+1,n,dp)+dyanamicrec(i+2,n,dp);
-    }
+    // int dyanamicrec(int i,int n,vector<int> &dp){
+    //     if(i==n)
+    //         return 1;
+    //     if(i>n)
+    //         return 0;
+    //     if(dp[i]!=-1)
+    //         return dp[i];
+    //     return dp[i]=dyanamicrec(i+1,n,dp)+dyanamicrec(i+2,n,dp);
+    // }
 public:
     int climbStairs(int n) {
         vector<int>dp(n+2,-1);
-        return dyanamicrec(0,n,dp);
+        dp[n]=1;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+        return dp[0];
     }
 };
