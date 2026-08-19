5ed59db05d25a0bd2f351827d10573b0aa7a922f
sandipan <sandipan2097@gmail.com>
2026-08-19 10:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (89.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -9,13 +9,23 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n+1,-1);
-        // return memoization(cost,dp,n);
-        dp[0]=0,dp[1]=0;
+        // vector<int> dp(n+1,-1);
 
+        // return memoization(cost,dp,n);
+
+        //tabulation
+        // dp[0]=0,dp[1]=0;
+
+        // for(int i=2;i<n+1;i++){
+        //     dp[i]=min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+        // }
+        // return dp[n];
+        int two_jump=0,one_jump=0;
         for(int i=2;i<n+1;i++){
-            dp[i]=min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+            int curr=min(cost[i-1]+one_jump,cost[i-2]+two_jump);
+            two_jump=one_jump;
+            one_jump=curr;
         }
-        return dp[n];
+        return one_jump;
     }
 };
