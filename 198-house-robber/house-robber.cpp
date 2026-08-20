7e034a13f3f5362877878f482e9e4871aea92ac2
sandipan <sandipan2097@gmail.com>
2026-08-20 20:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (47.13%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,13 @@
 class Solution {
-    int memoization(vector<int> &nums,vector<int> &dp,int idx){
-        if(idx==0)
-            return nums[0];
-        if(idx==1)
-            return max(nums[0],nums[1]);
-        if(dp[idx]!=-1)
-            return dp[idx];
-        return dp[idx]=max(nums[idx]+memoization(nums,dp,idx-2),memoization(nums,dp,idx-1));
-    }
+    // int memoization(vector<int> &nums,vector<int> &dp,int idx){
+    //     if(idx==0)
+    //         return nums[0];
+    //     if(idx==1)
+    //         return max(nums[0],nums[1]);
+    //     if(dp[idx]!=-1)
+    //         return dp[idx];
+    //     return dp[idx]=max(nums[idx]+memoization(nums,dp,idx-2),memoization(nums,dp,idx-1));
+    // }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
@@ -15,6 +15,13 @@ public:
             return nums[0];
             
         vector<int> dp(n,-1);
-        return memoization(nums,dp,n-1);
+        // return memoization(nums,dp,n-1);
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
