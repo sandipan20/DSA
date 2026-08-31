d2600c240e6c9bc78f10eb497728536a31c49070
sandipan <sandipan2097@gmail.com>
2026-08-31 23:13:10 +0530

    Time: 3 ms (85.90%) | Memory: 8.7 MB (59.67%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans=0,n=s.length();
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            while(l>=0&&r<n&&s[l]==s[r]){//odd len
+                ans++;
+                l--;
+                r++;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r]){//even len
+                ans++;
+                l--;
+                r++;
+            }
+        }
+        return ans;
+    }
+};
