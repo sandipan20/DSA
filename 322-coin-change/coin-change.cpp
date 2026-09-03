7dbc80d89f76c81a38ad525250cc342d1c0ce5fe
sandipan <sandipan2097@gmail.com>
2026-09-03 18:56:24 +0530

    Time: 12 ms (96.26%) | Memory: 17.8 MB (94.18%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,29 +13,41 @@ class Solution {
     //     return ans;
     // }
 
-    int memoization(vector<int>& coins, int amount,vector<int> &dp){
-        if(amount==0)
-            return 0;
-        if(amount<0)
-            return INT_MAX;
-        if(dp[amount]!=-1)
-            return dp[amount];
+    // int memoization(vector<int>& coins, int amount,vector<int> &dp){
+    //     if(amount==0)
+    //         return 0;
+    //     if(amount<0)
+    //         return INT_MAX;
+    //     if(dp[amount]!=-1)
+    //         return dp[amount];
 
-        int ans=INT_MAX;
-        for(int i:coins){
-            int result=memoization(coins,amount-i,dp);
-            if(result!=INT_MAX)
-                ans=min(ans,result+1);
-        }
-        return dp[amount]=ans;
-    }
+    //     int ans=INT_MAX;
+    //     for(int i:coins){
+    //         int result=memoization(coins,amount-i,dp);
+    //         if(result!=INT_MAX)
+    //             ans=min(ans,result+1);
+    //     }
+    //     return dp[amount]=ans;
+    // }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
+        // vector<int> dp(amount+1,-1);
         // int ans=rec(coins,amount);
-        int ans= memoization(coins,amount,dp);
-        if(ans==INT_MAX)
-            return -1;
-        return ans;
+        // int ans= memoization(coins,amount,dp);
+        // if(ans==INT_MAX)
+        //     return -1;
+        // return ans;
+
+        //tabulation
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<amount+1;i++){
+            for(int c:coins){
+                if(i-c>=0){
+                    dp[i]=min(dp[i],dp[i-c]+1);
+                }
+            }
+        }
+        return dp[amount]==amount+1 ?-1:dp[amount];
     }
 };
