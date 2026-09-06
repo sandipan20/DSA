b5ff7d4350c73b9b619db1f3471b9df13cb00dd7
sandipan <sandipan2097@gmail.com>
2026-09-06 22:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (82.65%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -16,22 +16,22 @@ class Solution {
     };
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,compare>q;
+        priority_queue<ListNode*,vector<ListNode*>,compare>q;//custom prority que which compares the elements diffrently
 
-        for(ListNode* head:lists){//putting head in the prority queue
+        for(ListNode* head:lists){//putting all head in the prority queue
             if(head){
                 q.push(head);
             }
         }
-        ListNode* temp=new ListNode(0);
+        ListNode* temp=new ListNode(0);//taking a temp linked list where we can add the next lowest element in sequence .
         ListNode* tail=temp;
         
-        while(!q.empty()){
+        while(!q.empty()){// adding element in temp
             ListNode* low=q.top();
             q.pop();
             tail->next=low;
             tail=tail->next;
-            if(low->next){
+            if(low->next){// if the next of the recently added element exist then add in the heap
                 q.push(low->next);
             }
         }
