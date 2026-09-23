830b5dc14cd2b2f15a47a253b72dec723cfd9237
sandipan <sandipan2097@gmail.com>
2026-09-23 10:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 34.2 MB (98.95%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return nullptr;
+        if(key>root->val){
+            root->right=deleteNode(root->right,key);
+        } else if(key<root->val){
+            root->left=deleteNode(root->left,key);
+        } else{
+            if(!root->left)
+                return root->right;
+            if(!root->right)
+                return root->left;
+            //if both exist we replace using either the greatest in left sub tree or smallest in the right sub tree. since for smallerst in right sub tree we traverse least we go for smallest in right;
+            TreeNode* temp=root->right;
+            while(temp->left){
+                temp=temp->left;
+            }
+            root->val=temp->val;
+            root->right=deleteNode(root->right,temp->val);
+        }
+        return root;
+    }
+};
